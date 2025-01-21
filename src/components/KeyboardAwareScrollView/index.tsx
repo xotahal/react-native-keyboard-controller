193c49d73baa2dfd9bcebffab079d6beeb098627
Jiří Otáhal <xotahal@gmail.com>
2025-01-21 11:09:43 +0700

    feat: add ability to turn off adding extra padding for keyboard
    # Conflicts:
    #	src/components/KeyboardAwareScrollView/index.tsx

--- src/components/KeyboardAwareScrollView/index.tsx
+++ src/components/KeyboardAwareScrollView/index.tsx
@@ -40,6 +40,8 @@ export type KeyboardAwareScrollViewProps = {
   enabled?: boolean;
   /** Adjusting the bottom spacing of KeyboardAwareScrollView. Default is `0`. */
   extraKeyboardSpace?: number;
+  /** Specifies whether to add padding equal to the keyboard height. This is useful when working with components like a bottom sheet, where the bottom sheet manages the position of the modal above the keyboard. In such cases, adding extra padding for the keyboard is unnecessary. */
+  withKeyboardHeightPadding?: boolean;
   /** Custom component for `ScrollView`. Default is `ScrollView`. */
   ScrollViewComponent?: React.ComponentType<ScrollViewProps>;
 } & ScrollViewProps;
@@ -111,6 +113,7 @@ const KeyboardAwareScrollView = forwardRef<
       extraKeyboardSpace = 0,
       ScrollViewComponent = Reanimated.ScrollView,
       snapToOffsets,
+      withKeyboardHeightPadding = true,
       ...rest
     },
     ref,
@@ -403,7 +406,7 @@ const KeyboardAwareScrollView = forwardRef<
         onLayout={onScrollViewLayout}
       >
         {children}
-        {enabled && <Reanimated.View style={view} />}
+        {withKeyboardHeightPadding && <Reanimated.View style={view} />}
       </ScrollViewComponent>
     );
   },
